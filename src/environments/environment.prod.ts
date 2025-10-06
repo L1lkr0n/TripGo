1cb7418c7a224eef91ef884b61fd4ed6a5a06ebc
L1lkr0n <alej.martinez@duocuc.cl>
2025-10-06 13:51:12 -0300

    agregar a enviroments.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   weatherApiKey: 'fdb9df792e8c62ebc883503d40ad6f0a',
-  apiKey: 'AIzaSyDgNydvoNuyKCaY4FKUhKTaJ3PJ9qt_T48'
+  googleMapsApiKey: 'AIzaSyBP4K2KWA5sY46N-dVObtTiInuH5S-MXyA',
 };
