c4d899b3b28088f203f053d77d2f4c5d22e8de3a
L1lkr0n <alej.martinez@duocuc.cl>
2025-10-06 15:33:18 -0300

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./pages/tabs-folder/tabs/tabs.routes').then((m) => m.routes),
   },
-   {
+  {
     path: 'flujo-itinerario',
       loadComponent: () => import('./pages/flujo-itinerario/flujo-itinerario.component')
       .then(m => m.FlujoItinerarioComponent)
