6c5b635ef6e8c4c4afa7c0536b27c29bbc5e37be
L1lkr0n <alej.martinez@duocuc.cl>
2025-10-07 19:05:49 -0300

    firebase locked in

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient()
+    provideHttpClient(), provideFirebaseApp(() => initializeApp({ projectId: "tripgo-473722", appId: "1:274317341753:web:df2db939821dca824ec9e8", storageBucket: "tripgo-473722.firebasestorage.app", apiKey: "AIzaSyA7Fen_mTkdzDY7spPh0Kp-uU4Hn7INNrY", authDomain: "tripgo-473722.firebaseapp.com", messagingSenderId: "274317341753", measurementId: "G-RBPQ0N0PXF", projectNumber: "274317341753", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideVertexAI(() => getVertexAI())
   ],
 });
