1857cfd10b396354711771aaaa73812671738b64
L1lkr0n <alej.martinez@duocuc.cl>
2025-10-14 20:04:02 -0300

    reinsertar android y modificar xml

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login/login.page';
 export const routes: Routes = [
-   {
-    //ruta prinncipal
-    path: '',
-    loadChildren: () => import('./pages/tabs-folder/tabs/tabs.routes').then((m) => m.routes),
-  },
-  {
-    //ruta al itinerario
-    path: 'flujo-itinerario',
-      loadComponent: () => import('./pages/flujo-itinerario/flujo-itinerario.component')
-      .then(m => m.FlujoItinerarioComponent)
-  }
-  /*
+
   {
     path: '',
     redirectTo: 'login',
@@ -25,6 +14,18 @@ export const routes: Routes = [
   {
     path: 'tabs',
     loadChildren: () => import('./pages/tabs-folder/tabs/tabs.routes').then(m => m.routes)
+  },
+  {
+    //ruta al itinerario
+    path: 'flujo-itinerario',
+      loadComponent: () => import('./pages/flujo-itinerario/flujo-itinerario.component')
+      .then(m => m.FlujoItinerarioComponent)
+  }
+  /*
+   {
+    //ruta prinncipal
+    path: '',
+    loadChildren: () => import('./pages/tabs-folder/tabs/tabs.routes').then((m) => m.routes),
   },
   */
 ];
